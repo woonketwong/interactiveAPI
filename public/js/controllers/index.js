288db81a281a3a3cbb09ea4dad83b820787c80d6
Woon Ket Wong <woonketwong@hotmail.com>
2013-12-18 12:37:06 -0800

    link click even to launching http request

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,12 +1,17 @@
 angular.module('mean.system').controller('IndexController', ['$scope', '$http', 'Global', function ($scope, $http, Global) {
     $scope.global = Global;
-    $scope.response = "testing123";
+    $scope.method = "GET";
+		//http://datsy-dev.azurewebsites.net/search/tag
 
-    $http({method: 'GET', url: 'http://datsy-dev.azurewebsites.net/search/tag'})
-	    .success(function(data, status, headers, config){
-	    	$scope.response = data;
-	    })
-	    .error(function(data, status, headers, config){
-	    	$scope.response = "Request Error!";
-	    });
+		$scope.httpRequest = function(){
+			var url = "http://" + $scope.url;
+			var method = $scope.method;
+	    $http({method: method, url: url})
+		    .success(function(data, status, headers, config){
+		    	$scope.response = data;
+		    })
+		    .error(function(data, status, headers, config){
+		    	$scope.response = "Request Error!";
+		    });
+	  }
 }]);
